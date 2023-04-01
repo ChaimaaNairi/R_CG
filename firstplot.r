56c2b376939db097a232571b317be12695209094
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-01 04:37:10 +0300

    commit

--- firstplot.r
+++ firstplot.r
@@ -3,3 +3,10 @@ line2 <- c(2,5,7,8,9,10)
 
 plot(line1, type = "l", col = "blue")
 lines(line2, type="l", col = "red")
+
+
+# Create a vector of pies
+x <- c(10,20,30,40)
+
+# Display the pie chart
+pie(x)
