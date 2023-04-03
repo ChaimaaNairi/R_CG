82319fb400f1fe386711b5857d722e6ef691507c
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-03 21:15:48 +0300

    commit

--- plot.r
+++ plot.r
@@ -2,22 +2,4 @@ x <- c(1, 2, 3, 4, 5)
 y <- c(3, 7, 8, 9, 12)
 
 plot(1:10, main="My Graph", xlab="The x-axis", ylab="The y axis")
-# numeric
-x <- 10.5
-class(x)
 
-# integer
-x <- 1000L
-class(x)
-
-# complex
-x <- 9i + 3
-class(x)
-
-# character/string # nolint
-x <- "R is exciting"
-class(x)
-
-# logical/boolean # nolint
-x <- TRUE
-class(x)
