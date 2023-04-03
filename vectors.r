9851bc99f1bd3b57d8ac8f8e6ffcca8044c45bb7
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-03 21:22:30 +0300

    commit

--- vectors.r
+++ vectors.r
@@ -8,6 +8,6 @@ numbers
 numbers1 <- 1.5:6.5
 numbers1
 
-# Vector with numerical decimals in a sequence where the last element is not used
+# Vector with numerical decimals in a sequence where the last element is not used # nolint
 numbers2 <- 1.5:6.3
 numbers2
