86485212f3bbe0b22bc7be27d6927788096577f2
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-03 21:22:46 +0300

    commit

--- vectors.r
+++ vectors.r
@@ -11,3 +11,9 @@ numbers1
 # Vector with numerical decimals in a sequence where the last element is not used # nolint
 numbers2 <- 1.5:6.3
 numbers2
+
+# Vector of logical values
+log_values <- c(TRUE, FALSE, TRUE, FALSE)
+
+log_values
+
